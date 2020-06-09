5852904b53a312637e31ede17ba80d0d7db38f7c
Tom Banish <tombanish@gmail.com>
2020-06-09 01:05:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -21,16 +21,16 @@ INSERT INTO users (name, age) VALUES ("Kevin", 32);
 INSERT INTO users (name, age) VALUES ("Mark", 31);
 
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "A", 1, 6/1/20, 7/1/20);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "A", 1, 6/1/20, 7/1/20);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "A", 1, 6/1/20, 7/1/20);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "A", 1, 6/1/20, 7/1/20);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "A", 1, 6/1/20, 7/1/20);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "A", 1, 6/1/20, 7/1/20);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "A", 1, 6/1/20, 7/1/20);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "A", 1, 6/1/20, 7/1/20);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "A", 1, 6/1/20, 7/1/20);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "A", 1, 6/1/20, 7/1/20);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "A", 100, 6/1/20, 7/1/20);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cat", "B", 200, 8/1/20, 9/1/20);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Bear", "C", 300, 10/1/20, 11/1/20);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Bat", "D", 400, 12/1/20, 1/1/21);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Frog", "E", 500, 2/1/21, 3/1/21);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Wolf", "F", 600, 4/1/21, 5/1/21);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Mouse", "G", 700, 6/1/10, 7/1/21);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "H", 800, 8/1/21, 9/1/21);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Owl", "I", 900, 10/1/21, 11/1/21);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Monkey", "J", 1000, 12/1/21, 1/1/22);
 
 
 
